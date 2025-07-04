615cb10c8bcc725027f51a80822599ba67d49e3e
Sujal Khadka <khadkasujal136@gmail.com>
2025-07-05 00:32:38 +0545

    Tokenizer V2 Commit

---
+++ tokenizer.py
@@ -0,0 +1,69 @@
+import os
+import re
+
+# First Reading the File
+file_path = 'the_verdict.txt'
+
+
+with open(file_path, 'r', encoding='utf-8') as f:
+    raw_text = f.read()
+
+result = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in result if item.strip()]
+
+
+# Building a Tokenizer
+
+all_words = sorted(set(preprocessed))
+all_words.extend(['<|unk|>', '<|endoftext|>'])  # Adding special tokens
+vocab = {token:integer for integer,token in enumerate(all_words)}
+
+#Putting it all in a Tokenizer and DeTokenizer Class
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int else '<|unk|>' for item in preprocessed]
+        
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replacing spaces before punctuations
+        text = re.sub(r'\s+([,.:;?_!"()\'])', r'\1', text)
+        return text
+
+text1 = "Hello, Do you like tea?"
+text2 = "In the sunlit terraces of the palace"
+text = " <|endoftext|> ".join([text1, text2])
+
+
+tokenizer = SimpleTokenizerV2(vocab)
+encoded_text = tokenizer.encode(text)
+
+print(tokenizer.decode(encoded_text))
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
